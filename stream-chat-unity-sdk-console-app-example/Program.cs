1151ed69990a51d8fd569e4df5a06c7c86589732
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2022-05-10 16:01:28 +0200

    Added example implementation which sends 2 messages with random number and prints all messages in the channel

--- stream-chat-unity-sdk-console-app-example/Program.cs
+++ stream-chat-unity-sdk-console-app-example/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace stream_chat_unity_sdk_console_app_example
+﻿using System;
+using StreamChat.Libs.Auth;
+
+namespace ToDeleteTestStreamSDKInConsole
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+
+            try
+            {
+                var authCredentials = new AuthCredentials(
+                    apiKey: "",
+                    userId: "",
+                    userToken: "");
+
+                if (authCredentials.IsAnyEmpty())
+                {
+                    Console.WriteLine("Please provide valid credentials: API_KEY, USER_ID, USER_TOKEN");
+                    Console.ReadKey();
+                    return;
+                }
+
+                _streamChatService = new StreamChatService(authCredentials);
+                _isRunning = true;
+
+                while (_isRunning)
+                {
+                    var currentTimestampMs = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+
+                    if (currentTimestampMs < _nextFrameTimestampMs)
+                    {
+                        continue;
+                    }
+
+                    var deltaTime = (currentTimestampMs - _lastFrameTimestampMs) / 1000;
+                    _streamChatService.Update((float)deltaTime);
+
+                    _lastFrameTimestampMs = currentTimestampMs;
+                    _nextFrameTimestampMs = _lastFrameTimestampMs + MsPerFrame;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private const float FrameRate = 30;
+        private const float MsPerFrame = 1000 / 30f;
+
+        private static StreamChatService _streamChatService;
+
+        private static bool _isRunning;
+        private static double _lastFrameTimestampMs;
+        private static double _nextFrameTimestampMs;
+
+        private static void OnProcessExit(object sender, EventArgs e)
+        {
+            Console.WriteLine("On Exit");
+            _isRunning = false;
+
+            _streamChatService.Dispose();
         }
     }
 }
---
+++ stream-chat-unity-sdk-console-app-example/StreamChatService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading.Tasks;
+using StreamChat.Core;
+using StreamChat.Core.Models;
+using StreamChat.Core.Requests;
+using StreamChat.Libs.Auth;
+
+namespace ToDeleteTestStreamSDKInConsole
+{
+    public class StreamChatService : IDisposable
+    {
+        public StreamChatService(AuthCredentials authCredentials)
+        {
+            _client = StreamChatClient.CreateDefaultClient(authCredentials);
+            _client.Connected += OnClientConnected;
+            _client.Connect();
+
+            Console.WriteLine("Client created. Attempt to connect.");
+        }
+
+        public void Update(float deltaTime)
+        {
+            _client.Update(deltaTime);
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+        }
+
+        private const string ChannelType = "messaging";
+        private const string ChannelId = "main-chat-id";
+
+        private readonly IStreamChatClient _client;
+
+        private void OnClientConnected()
+        {
+            Console.WriteLine("Client Connected.");
+
+            TestServiceAsync()
+                .ContinueWith(_ => Console.WriteLine(_.Exception), TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        /// <summary>
+        /// Create 2 sample messages and print latest messages
+        /// </summary>
+        private async Task TestServiceAsync()
+        {
+            var rnd = new Random();
+
+            var randomNumber = rnd.Next();
+            Console.WriteLine("Send test message with a random number: " + randomNumber);
+            await SendNewMessageAsync(randomNumber.ToString());
+
+            randomNumber = rnd.Next();
+            Console.WriteLine("Send test message with a random number: " + randomNumber);
+            await SendNewMessageAsync(randomNumber.ToString());
+
+            var channelState = await FetchLatestMessagesAsync();
+
+            PrintMessages(channelState);
+        }
+
+        private async Task SendNewMessageAsync(string message)
+        {
+            try
+            {
+                var response = await _client.MessageApi.SendNewMessageAsync(ChannelType, ChannelId, new SendMessageRequest
+                {
+                    Message = new MessageRequest
+                    {
+                        Text = message
+                    }
+                });
+
+                Console.WriteLine($"Message: `{response.Message.Text}` sent successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private async Task<ChannelState> FetchLatestMessagesAsync()
+        {
+            try
+            {
+                return await _client.ChannelApi.GetOrCreateChannelAsync(ChannelType, ChannelId,
+                    new ChannelGetOrCreateRequest
+                    {
+                        State = true,
+                        Watch = true,
+                    });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void PrintMessages(ChannelState channelState)
+        {
+            Console.WriteLine($"Channel: `{channelState.Channel.Name}` last messages:");
+            foreach (var msg in channelState.Messages)
+            {
+                Console.WriteLine($"{msg.Text}");
+                Console.WriteLine($"Author: {msg.User.Id}");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
